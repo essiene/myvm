c4b63532056c708064e63ce252d46eac7da0a298
Essien Ita Essien <essien.ita@uknglobal.com>
2009-05-19 15:18:08 +0100

    Add vminst to the repository.
    vminst is the module that helps deal with vm instructions as
    an abstract data type

---
+++ vminst.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <vmfile.h>
+
+struct _VmInst {
+    unsigned char opcode;
+    unsigned char source;
+    unsigned char target;
+};
+
+VmInst vminst_new(int fd, unsigned int pos)
+{
+    VmInst inst = calloc(sizeof(*inst), 1);
+    if(inst == NULL) {
+        vmerrno = VM_ENEWINST;
+        return NULL;
+    }
+
+    ssize_t bytes = read(fd, &inst->opcode, 1);
+    if(bytes != 1) {
+        vmerrno = VM_EREADOPCODE;
+        return NULL;
+    }
+
+    bytes = read(fd, &inst->source, 1);
+    if(bytes != 1) {
+        vmerrno = VM_EREADSOURCE;
+        return NULL;
+    }
+
+    bytes = read(fd, &inst->target, 1);
+    if(bytes != 1) {
+        vmerrno = VM_EREADTARGET;
+        return NULL;
+    }
+
+    return inst;
+
+}
+
+void vminst_print(VmInst vminst)
+{
+    printf("%.2X%.2X%.2X\n", vminst->opcode, vminst->source, vminst->target);
+}
+
+void vminst_del(VmInst vminst)
+{
+    free(vminst);
+}
