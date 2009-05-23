3134c16798e0dfbf215a7ad810b6909ef3581988
Essien Ita Essien <essien.ita@uknglobal.com>
2009-05-24 00:48:33 +0100

    Added vm module and ops.c which contain operations

---
+++ ops.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <myvm.h>
+
+
+VmState op_ldr(VmState state, VmInst inst)
+{
+    enum Register target = vminst_target_get(inst);
+    vmstate_general_set(state, target, vminst_source_get(inst));
+
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+
+VmState op_add(VmState state, VmInst inst)
+{
+    enum Register target = vminst_target_get(inst);
+    enum Register source = vminst_source_get(inst);
+
+    unsigned char newval = vmstate_general_get(state, target) + vmstate_general_get(state, source);
+    vmstate_general_set(state, target, newval);
+
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+
+VmState op_sub(VmState state, VmInst inst)
+{
+    enum Register target = vminst_target_get(inst);
+    enum Register source = vminst_source_get(inst);
+
+    unsigned char newval = vmstate_general_get(state, target) - vmstate_general_get(state, source);
+    vmstate_general_set(state, target, newval);
+
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+VmState op_mul(VmState state, VmInst inst)
+{
+    enum Register target = vminst_target_get(inst);
+    enum Register source = vminst_source_get(inst);
+
+    unsigned char newval = vmstate_general_get(state, target) * vmstate_general_get(state, source);
+    vmstate_general_set(state, target, newval);
+
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+
+VmState op_div(VmState state, VmInst inst)
+{
+    enum Register target = vminst_target_get(inst);
+    enum Register source = vminst_source_get(inst);
+
+    unsigned char newval = vmstate_general_get(state, target) / vmstate_general_get(state, source);
+    vmstate_general_set(state, target, newval);
+
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+VmState op_in(VmState state, VmInst inst)
+{
+    int n;
+    printf("[myvm <input>] $ ");
+    scanf("%d", &n);
+    vmstate_general_set(state, vminst_source_get(inst), (unsigned char) n);
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+VmState op_out(VmState state, VmInst inst)
+{
+    enum Register reg = vmstate_general_get(state, vminst_source_get(inst));
+
+    printf("%d\n", reg);
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+VmState op_err(VmState state, VmInst inst)
+{
+    write(2, "%d\n", vmstate_general_get(state, vminst_source_get(inst)));
+    state = vmstate_ip_inc(state);
+    return state;
+}
---
+++ vm.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <myvm.h>
+
+struct _Vm {
+    VmFile file;
+    VmState state;
+};
+
+static VmState vm_exec(VmState, VmInst);
+
+Vm vm_new(VmFile file, VmState state)
+{
+    Vm vm = calloc(1, sizeof(*vm));
+    if(vm == NULL) {
+        vmerrno = VM_EVMCREATEERR;
+        return NULL;
+    }
+
+    vm->file = file;
+    vm->state = state;
+
+    return vm;
+
+}
+
+Vm vm_start(Vm vm)
+{
+    while(1) {
+        unsigned int next_addr = vmstate_ip_get(vm->state);
+
+        VmInst inst = vmfile_inst_fetch(vm->file, next_addr);
+        if(inst == NULL) {
+            break;
+        }
+
+        vm->state = vm_exec(vm->state, inst);
+    }
+
+    return vm;
+}
+
+void vm_del(Vm vm)
+{
+    free(vm);
+}
+
+
+/* Private Funcs */
+
+VmState vm_exec(VmState state, VmInst inst)
+{
+    switch(vminst_opcode_get(inst)) {
+        case LDR: 
+            state = op_ldr(state, inst);break;
+        case ADD:
+            state = op_add(state, inst);break;
+        case SUB:
+            state = op_sub(state, inst);break;
+        case MUL:
+            state = op_mul(state, inst);break;
+        case DIV:
+            state = op_div(state, inst);break;
+        case IN:
+            state = op_in(state, inst);break;
+        case OUT:
+            state = op_out(state, inst);break;
+        case ERR:
+            state = op_err(state, inst);break;
+        default: break;
+    }
+
+    /*vmstate_print(state);*/
+    return state;
+}
