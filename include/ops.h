e3e26b6903c086c540b481f520a98c53ce407813
Essien Ita Essien <essiene@gmail.com>
2009-05-24 23:46:37 +0100

    Implement new vm operations
    * op_cmp - Compare two registers
    * op_jmp - Unconditional jump to an instruction
    * op_jpe - Jump to an instruction if equal flag is set
    * op_inc - Increment a register
    
    Not yet hooked up to the vm

--- include/ops.h
+++ include/ops.h
@@ -12,5 +12,9 @@ VmState op_div(VmState, VmInst);
 VmState op_in(VmState, VmInst);
 VmState op_out(VmState, VmInst);
 VmState op_err(VmState, VmInst);
+VmState op_cmp(VmState, VmInst);
+VmState op_jmp(VmState, VmInst);
+VmState op_jpe(VmState, VmInst);
+VmState op_inc(VmState, VmInst);
 
 #endif
--- ops.c
+++ ops.c
@@ -88,3 +88,66 @@ VmState op_err(VmState state, VmInst inst)
     state = vmstate_ip_inc(state);
     return state;
 }
+
+VmState op_cmp(VmState state, VmInst inst)
+{
+    enum Register source = vminst_source_get(inst);
+    enum Register target = vminst_target_get(inst);
+
+    unsigned char source_value = vmstate_general_get(state, source);
+    unsigned char target_value = vmstate_general_get(state, target);
+
+    if(source_value == target_value) {
+        state = vmstate_flag_equal_set_true(state);
+        state = vmstate_flag_greater_set_false(state);
+        goto CMP_OK;
+    }
+
+    state = vmstate_flag_equal_set_false(state);
+
+    if(source_value > target_value) {
+        state = vmstate_flag_greater_set_true(state);
+        goto CMP_OK;
+    }
+
+    state = vmstate_flag_greater_set_false(state);
+
+CMP_OK:
+    state = vmstate_ip_inc(state);
+    return state;
+}
+
+VmState op_jmp(VmState state, VmInst inst)
+{
+    unsigned int addr = vminst_source_get(inst);
+
+    state = vmstate_ip_set(state, addr);
+    return state;
+
+}
+
+VmState op_jpe(VmState state, VmInst inst)
+{
+    unsigned int addr = vminst_source_get(inst);
+
+    if(vmstate_flag_equal_get(state) == 0x0) {
+        state = vmstate_ip_inc(state);
+        return state;
+    }
+
+    state = vmstate_ip_set(state, addr);
+    return state;
+
+}
+
+VmState op_inc(VmState state, VmInst inst)
+{
+    enum Register reg = vminst_source_get(inst);
+    unsigned char val = vmstate_general_get(state, reg);
+
+    val += 1;
+
+    state = vmstate_general_set(state, reg, val);
+    state = vmstate_ip_inc(state);
+    return state;
+}
