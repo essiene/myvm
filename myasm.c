3b7e689b98536328d82ebed8f781b6a2978afba8
Essien Ita Essien <essiene@gmail.com>
2009-05-31 22:04:04 +0100

    Hook up tokenizer to assembler frontend.
    We can now open a file, pass it to the (incomplete) tokenizer
    and see what happens

--- myasm.c
+++ myasm.c
@@ -2,6 +2,7 @@
 #include <libgen.h>
 #include <fcntl.h>
 
+#include <myvm.h>
 
 int main(int argc, char** argv)
 {
@@ -9,6 +10,17 @@ int main(int argc, char** argv)
         printf(" Usage: %s <file>\n", basename(argv[0]));
         return 1;
     }
+
+    char* filename = argv[1];
+
+    int fp = open(filename, O_RDONLY);
+    if(fp == -1) {
+        perror("Opening source file");
+        return 1;
+    }
+
+    int count = myasm_tokenize(fp);
+    printf("Processed %d tokens\n", count);
 
     return 0;
 }
