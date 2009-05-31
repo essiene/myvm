4ec38e226f0f7bf0022137ab1b6368c21e40c1c0
Essien Ita Essien <essiene@gmail.com>
2009-05-31 21:43:19 +0100

    Implement stream reader for reading next character
    The reader properly sets the Symbol when various things
    happen, like EOF is reached, or there is an error during
    reading. It also advances the various pointers, so that
    we can have accurate information when reporting errors

--- myasm_lexer.c
+++ myasm_lexer.c
@@ -8,3 +8,36 @@ typedef struct {
     int linenum;
     int linepos;
 } Symbol;
+
+Symbol mgetchar(Symbol, int);
+
+Symbol mgetchar(Symbol sym, int fd)
+{
+    unsigned char c;
+
+    ssize_t bytes = read(fd, &c, sizeof(c));
+    if(bytes == -1) {
+        perror("Problem reading file");
+        goto problem;
+    }
+
+    if(bytes == 0) {
+        printf("End of file reached\n");
+        goto problem;
+    }
+
+    if(c == '\n') { /*TODO: deal properly with newline on different OSes*/
+        sym.linenum += 1;
+        sym.linepos = 0;
+    } else {
+        sym.linepos += 1;
+    }
+
+    sym.val = c;
+    sym.count += 1;
+    return sym;
+
+    problem:
+        sym.val = -1;
+        return sym;
+}
