a83ffefe55367c415a9cecd619c987c541fa73e7
Essien Ita Essien <essiene@gmail.com>
2009-06-01 16:27:08 +0100

    Update simple assembly example
    To reflect the new tokenizer ability. Basically, as at now,
    whitespace is insignificant, and token borders are properly
    identified... yay!!!

--- example/simple.asm
+++ example/simple.asm
@@ -1,12 +1,11 @@
-ldr $100 %a;
-ldr $50 %b;
-ldr $15 %c;
-in %d;
-in %e;
-in %f;
-out %a;
-out %b;
-out %c;
-out %d;
-out %e;
-out %f;
+ldr $100 %a
+ldr $50 %b
+ldr$15%c
+in %d
+in %e
+in %f
+out%aout %b
+out %c
+out %d
+out %e
+out %f
